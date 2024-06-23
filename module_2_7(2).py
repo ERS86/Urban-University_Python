077ea11b77120360e7b57331e7e0efb9f6fd4089
ERS86 <99912866+ERS86@users.noreply.github.com>
2024-06-23 15:31:39 +0300

    Update module_2_7(2).py
    Скопировал код из PyCharm

--- module_2_7(2).py
+++ module_2_7(2).py
@@ -1,1 +1,17 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    def multiply_digits(s):
+        if not s:  #если строка пуста
+            return "Нет числа"
+        elif len(s) == 1:  #если в строке одна цифра
+            return int(s)
+        else:
+            return int(s[0]) * multiply_digits(s[1:]) if s[0] != '0' else multiply_digits(s[1:])
+    return multiply_digits(str_number)
 
+print(get_multiplied_digits('12345'))   #результат: 120
+print(get_multiplied_digits('1012'))    #результат: 2
+print(get_multiplied_digits('0'))       #результат: 0
+print(get_multiplied_digits('5'))       #результат: 5
+print(get_multiplied_digits(''))        #результат: Нет числа
+print(get_multiplied_digits('000123'))  #результат: 6

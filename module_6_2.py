a97a6d3e4643dee297a78312dd9953d0ba4dfec1
ERS86 <99912866+ERS86@users.noreply.github.com>
2024-11-04 23:25:02 +0300

    Update module_6_2.py
    Повторное решение

--- module_6_2.py
+++ module_6_2.py
@@ -1,49 +1,40 @@
-'''
-КАК!!!!!! РЕШИТЬ НЕСООТВЕТСТВИЕ В ПОСЛЕДОВАТЕЛЬНОСТИ МЕТОДОВ(модель,мощность,цвет)
-И ПОСЛЕДОВАТЕЛЬНОСТИ ПРИНИМАЕМЫХ ДАННЫХ (имя, модель, цвет, мощность)
-И ПОСЛЕДОВАТЕЛЬНОСТИ ВЫВОДА модель,мощность,цвет,имя!!!!!!!!
-
-заявленных в условии и примере
-????????????????????????????????????????????????????????????????????????????????????
-'''
-
 class Vehicle:
+    _COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 
-    def __init__(self, __model, __engine_power, __color, owner,):
-        self.owner = str(owner)
+    def __init__(self, __model, __engine_power, __color, __owner):
+        self.__owner = str(__owner)
         self.__model = str(__model)
         self.__engine_power = int(__engine_power)
         self.__color = str(__color)
 
-    __COLOR_VARIANTS = ('red', 'orange', 'yellow', 'green', 'blue', 'darkblue', 'violet', 'black')
-
     def get_model(self):
-        return self.__model
+        return f'"Модель: {self.__model}"'
 
     def get_horsepower(self):
-        return self.__engine_power
+        return f'"Мощность двигателя: {self.__engine_power}"'
 
     def get_color(self):
-        return self.__color
+        return f'"Цвет: {self.__color}"'
 
     def print_info(self):
-        print ( f'Модель: {self.__model},\n'
-                f'Мощность двигателя: {self.__engine_power},\n'
-                f'Цвет: {self.__color},\n'
-                f'Владелец: {self.owner}')
+        print   (f'"Модель: {self.__model}"\n'
+                f'"Мощность двигателя: {self.__engine_power}"\n'
+                f'"Цвет: {self.__color}"\n'
+                f'"Владелец: {self.__owner}"\n')
 
     def set_color(self, new_color):
-        if new_color.lower() in self.__COLOR_VARIANTS:
+        if new_color.lower() in self._COLOR_VARIANTS:
             self.__color = new_color
         else:
-            print(f'Нельзя сменить цвет на {new_color}')
+            print(f'"Нельзя сменить цвет на {new_color}"')
 
 
 class Sedan(Vehicle):
     __PASSENGERS_LIMIT = 5
 
+
 # Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
-vehicle1 = Sedan('Toyota Mark II',   500,'blue','Fedos')
+vehicle1 = Sedan('Toyota Mark II', 500, 'blue', 'Fedos')
 
 # Изначальные свойства
 vehicle1.print_info()

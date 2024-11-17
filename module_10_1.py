75c9e1ca9afdb7d0d783fcf65fc16726778951a5
ERS86 <99912866+ERS86@users.noreply.github.com>
2024-11-17 04:16:53 +0300

    Update module_10_1.py

--- module_10_1.py
+++ module_10_1.py
@@ -7,10 +7,18 @@ def write_words(word_count, file_name):
     with open(file_name, "a", encoding='utf-8') as file:
         for i in range(word_count):
             file.write(f'Какое-то слово № {i + 1}\n')
-            sleep(0.1)
+            sleep(0)
         file.close()
         print(f'Завершилась запись в файл {file_name}')
 
+
+current_time1 = datetime.datetime.now()
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+current_time2 = datetime.datetime.now()
+print(f'Работа потоков {current_time2 - current_time1}')
 
 current_time3 = datetime.datetime.now()
 thread1 = threading.Thread(target=write_words, args=(10, 'example5.txt'))
@@ -27,11 +35,3 @@ thread3.join()
 thread4.join()
 current_time4 = datetime.datetime.now()
 print(f'Работа потоков {current_time4 - current_time3}')
-
-current_time1 = datetime.datetime.now()
-write_words(10, 'example1.txt')
-write_words(30, 'example2.txt')
-write_words(200, 'example3.txt')
-write_words(100, 'example4.txt')
-current_time2 = datetime.datetime.now()
-print(f'Работа функций {current_time2 - current_time1}')
